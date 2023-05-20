137ce9ebbee0205fc77757b57f8c2ac8a0937137
ULTRAVIOLENC3 <VDimitriy@gmail.com>
2023-05-21 01:36:12 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -0,0 +1,28 @@
+package com.example.newsapp
+
+import android.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapp.databinding.ActivityNewsBinding
+
+
+class NewsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navigationView = binding.bottomNavigationView
+        val navController = binding.newsNavHostFragment.getFragment<NavHostFragment>().navController
+        navigationView.setupWithNavController(navController)
+
+    }
+}
+
+
+
