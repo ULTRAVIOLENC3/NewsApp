af459661f88ebe8b35d798f89b2156d56a698884
ULTRAVIOLENC3 <VDimitriy@gmail.com>
2023-06-10 10:20:27 +0300

    Added db, recyclerview adapter, refactored a bit

--- app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -3,13 +3,17 @@ package com.example.newsapp.ui
 import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.databinding.ActivityNewsBinding
+import com.example.newsapp.db.ArticleDatabase
+import com.example.newsapp.repository.NewsRepository
 
 
 class NewsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsBinding
+    lateinit var  viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,9 @@ class NewsActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
         val navigationView = binding.bottomNavigationView
         val navController = binding.newsNavHostFragment.getFragment<NavHostFragment>().navController
         navigationView.setupWithNavController(navController)
